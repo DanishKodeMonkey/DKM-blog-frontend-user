805757fb2627c665b764da7c09f4df761cb49abe
danishkodemonkey <humberjini@hotmail.com>
2024-06-03 17:44:06 +0200

    feat: set up file structure for project

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
-import './index.css';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
