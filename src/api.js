53ef98fbd0f629314224d5b8edc6378b38cf94f3
danishkodemonkey <humberjini@hotmail.com>
2024-06-04 14:11:39 +0200

    feat: add signup and sign in API functions

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
+/// BLOG POSTS ///
+// fetch all blog posts
 export const fetchAllBlogPosts = async () => {
     try {
         const response = await fetch(`${API_URL}/blog/posts`);
@@ -11,4 +13,34 @@ export const fetchAllBlogPosts = async () => {
         console.error('Error: fetching blog posts: ', error);
         throw error;
     }
+};
+
+/// USERS ///
+// Sign up new user
+
+export const createUser = async (user) => {
+    const response = await fetch(`${API_URL}/users/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user),
+    });
+    if (!response.ok) {
+        throw new Error('Sign up failed');
+    }
+    return response.json();
+};
+
+/// AUTH ///
+
+// Sign in user
+export const signIn = async (user) => {
+    const response = await fetch(`${API_URL}/auth/sign-in`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user),
+    });
+    if (!response.ok) {
+        throw new Error('Sign in failed');
+    }
+    return response.json();
 };
