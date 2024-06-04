17f2e38b318eb9cd42b455894200030a57c4c84f
danishkodemonkey <humberjini@hotmail.com>
2024-06-04 10:22:55 +0200

    fix: width fix on styling

--- src/components/header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return (
-        <div className='bg-gradient-to-r from-orange-200 from-5% via-orange-400 via-50%  to-orange-200 to-95% text-center h-full  pt-5'>
-            <h1 className='text-4xl h-full'>The danishKodeMonkey blog</h1>
-        </div>
-    );
-}
-
-export default Header;
