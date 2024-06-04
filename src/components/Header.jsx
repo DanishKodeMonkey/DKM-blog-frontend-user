ea25a942d35080c253123f4603788e00cb939cd1
danishkodemonkey <humberjini@hotmail.com>
2024-06-04 10:26:03 +0200

    fix: navbar attempt 1 failed, will retry

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div className='bg-gradient-to-r from-orange-200 from-5% via-orange-400 via-50%  to-orange-200 to-95% text-center h-full  pt-5 w-screen'>
+            <h1 className='text-4xl h-full'>The danishKodeMonkey blog</h1>
+        </div>
+    );
+}
+
+export default Header;
