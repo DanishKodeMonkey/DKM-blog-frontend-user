13db479011fb6da7246fc3f4d1985cfdd7f906bd
danishkodemonkey <humberjini@hotmail.com>
2024-06-04 13:40:15 +0200

    feat: fetchAllBlogPosts API function implemented, renders

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,24 +1,40 @@
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { fetchAllBlogPosts } from '../api';
 import DefaultArticle from '../components/DefaultArticle';
-import Popeye from '../components/Popeye';
-import Spinach from '../components/Spinach';
 
 const Blog = () => {
-    const { name } = useParams();
+    const [posts, setPosts] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const getPosts = async () => {
+            try {
+                const data = await fetchAllBlogPosts();
+                console.log(data);
+                setPosts(data);
+            } catch (err) {
+                setError(err.message);
+            }
+        };
+        getPosts();
+    }, []);
+    if (error) {
+        return <p>Error: {error}</p>;
+    }
+
+    if (posts.length === 0) {
+        return <DefaultArticle />;
+    }
 
     return (
         <div>
-            <h1>Hello from the blog page!</h1>
-            <p>Have a look at the various blog posts here!</p>
-
-            {name === 'popeye' ? (
-                <Popeye />
-            ) : name === 'spinach' ? (
-                <Spinach />
-            ) : (
-                <DefaultArticle />
-            )}
+            <h1>Blog posts</h1>
+            {posts.map((post) => (
+                <div key={post._id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.text}</p>
+                </div>
+            ))}
         </div>
     );
 };
