f4d2c527905491ae327f320c8d7dccb9905e36c1
danishkodemonkey <humberjini@hotmail.com>
2024-06-05 12:57:59 +0200

    ROUTER REFACTOR: refactor app as main render entrypoint for template

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
+import './styles/index.css';
+
+function App() {
+    return (
+        <div className='flex flex-col justify-between h-screen'>
+            <header className='h-20'>
+                <Header />
+            </header>
+            <main className='container mx-auto mb-auto grow'>
+                <Outlet />
+            </main>
+            <footer className='h-12'>
+                <Footer />
+            </footer>
+        </div>
+    );
+}
+
+export default App;
