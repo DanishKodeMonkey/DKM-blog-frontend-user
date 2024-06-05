6e32788b3bf308889a8e34762ae33d2cd08be89b
danishkodemonkey <humberjini@hotmail.com>
2024-06-05 13:03:17 +0200

    BREAKING: Create navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,41 @@
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <nav className='bg-gray-800 p-4 text-white'>
-            <ul className='flex space-x-4'>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/blog'>Blog</Link>
-                </li>
-                <li>
-                    <Link to='/signup'>Sign Up</Link>
-                </li>
-                <li>
-                    <Link to='/signin'>Sign In</Link>
-                </li>
-            </ul>
+        <nav>
+            <NavLink
+                to={'/'}
+                className={({ isActive, isPending }) =>
+                    isPending ? 'pending' : isActive ? 'active' : ''
+                }
+            >
+                Home
+            </NavLink>
+            <NavLink
+                to={'/blog'}
+                className={({ isActive, isPending }) =>
+                    isPending ? 'pending' : isActive ? 'active' : ''
+                }
+            >
+                Blog
+            </NavLink>
+            <NavLink
+                to={'/user/signin'}
+                className={({ isActive, isPending }) =>
+                    isPending ? 'pending' : isActive ? 'active' : ''
+                }
+            >
+                Sign in
+            </NavLink>
+            <NavLink
+                to={'/user/signup'}
+                className={({ isActive, isPending }) =>
+                    isPending ? 'pending' : isActive ? 'active' : ''
+                }
+            >
+                Sign up
+            </NavLink>
         </nav>
     );
 };
