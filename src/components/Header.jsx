03960b5343d237239ed1c88daf49578b94d8c2b7
danishkodemonkey <humberjini@hotmail.com>
2024-06-05 13:02:57 +0200

    feat: add functionining navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+import NavBar from './Navbar';
+
 function Header() {
     return (
         <div className='bg-gradient-to-r from-orange-200 from-5% via-orange-400 via-50%  to-orange-200 to-95% text-center h-full  pt-5 w-screen'>
-            <h1 className='text-4xl h-full'>The danishKodeMonkey blog</h1>
+            <div>
+                <h1 className='text-4xl h-full'>The danishKodeMonkey blog</h1>
+                <NavBar />
+            </div>
         </div>
     );
 }
