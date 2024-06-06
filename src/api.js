dc9c3ca3aa233a29bca23f175499c940a2a78e11
danishkodemonkey <humberjini@hotmail.com>
2024-06-06 14:18:52 +0200

    BREAKING: api handler for retrieving current user information

--- src/api.js
+++ src/api.js
@@ -1,13 +1,31 @@
+import { jwtDecode } from 'jwt-decode';
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 // Security: Check for, and include JWT token on API requests
 // Use for protected API routes.
 const getAuthHeaders = () => {
+    console.warn('getAuthHeaders trigger---');
     const token = localStorage.getItem('token');
     // Was a token found? If so attach, otherwise dont.
     return token ? { Authorization: `Bearer ${token}` } : {};
 };
 
+// Function to get user ID from the token
+const getUserIdFromToken = () => {
+    console.warn('getUserIdFromToken triggere---');
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        console.log('decoding token...');
+        const decodedToken = jwtDecode(token);
+        console.log('Token decoded');
+        return decodedToken.sub;
+    } else {
+        console.error('Token failed to retrieve');
+        return null;
+    }
+};
 /// BLOG POSTS ///
 // fetch all blog posts
 export const fetchAllBlogPosts = async () => {
@@ -36,6 +54,39 @@ export const createUser = async (user) => {
         throw new Error('Sign up failed');
     }
     return response.json();
+};
+
+// Fetch current users information
+export const fetchCurrentUser = async () => {
+    console.warn('FetchCurrentUser triggere ---');
+    const userId = getUserIdFromToken();
+    console.log('Received userId');
+    if (!userId) {
+        console.error('UserId from getUserIdFromToken failed');
+        throw new Error('User ID not found in token');
+    }
+    try {
+        console.warn('Attempting to fetch user with userId');
+        const response = await fetch(`${API_URL}/users/${userId}`, {
+            headers: {
+                ...getAuthHeaders(),
+                'Content-type': 'application/json',
+            },
+        });
+        if (!response.ok) {
+            console.error(
+                'response failed...',
+                response.status,
+                response.statusText
+            );
+            throw new Error('Failed to fetch user data');
+        }
+        console.log('Response valid, jsonifying...');
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching current user data: ', error);
+        throw error;
+    }
 };
 
 /// AUTH ///
