55a53a4b3289f5ca3dcc05cea1fe9d4dcd4ddcbe
danishkodemonkey <humberjini@hotmail.com>
2024-06-07 14:13:07 +0200

    feat: update blog page to accomidate new sidebar feature

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 import { fetchAllBlogPosts } from '../api';
 import DefaultArticle from '../components/DefaultArticle';
+import BlogSidebar from '../components/BlogSidebar';
 
 // custom hook
-const AllBlogPosts = () => {
+const Blog = () => {
     const [posts, setPosts] = useState([]);
+    const [selectedPost, setSelectedPost] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
 
@@ -22,24 +24,48 @@ const AllBlogPosts = () => {
         };
         getPosts();
     }, []);
-    return { posts, error, loading };
-};
-const Blog = () => {
-    const { posts, error, loading } = AllBlogPosts();
+
+    const handleSelectPost = (post) => {
+        setSelectedPost(post);
+    };
 
     if (loading) return <p>Loading... please wait.</p>;
     if (error) return <p>A network error was encountered.</p>;
     if (posts <= 0) return <DefaultArticle />;
 
     return (
-        <div>
-            <h1>Blog posts</h1>
-            {posts.map((post) => (
-                <div key={post._id}>
-                    <h2>{post.title}</h2>
-                    <p>{post.text}</p>
-                </div>
-            ))}
+        <div className='blog-container'>
+            <BlogSidebar
+                posts={posts}
+                onSelectPost={handleSelectPost}
+            />
+            <div className='main-content'>
+                <h1>Blog posts</h1>
+                {selectedPost ? (
+                    <div key={selectedPost._id}>
+                        <h2>{selectedPost.title}</h2>
+                        <p>{selectedPost.text}</p>
+                        <div>
+                            <h3>comments:</h3>
+                            {selectedPost.comments.map((comment) => (
+                                <div key={comment._id}>
+                                    <p>{comment.text}</p>
+                                    <p>{comment.timestamp}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        {posts.map((post) => (
+                            <div key={post._id}>
+                                <h2>{post.title}</h2>
+                                <p>{post.text}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
