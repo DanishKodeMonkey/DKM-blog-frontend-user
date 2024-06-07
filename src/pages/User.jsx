95f22e9fa7df98ab7c0cfef0636e93cf579f1725
danishkodemonkey <humberjini@hotmail.com>
2024-06-07 13:19:25 +0200

    BREAKING: refactor user page with populated fields that can be edited and updated

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react';
-import { fetchCurrentUser } from '../api';
+import { fetchCurrentUser, updateUser } from '../api';
 import AuthContext from '../AuthContext';
 
 const UserPage = () => {
@@ -26,18 +26,101 @@ const UserPage = () => {
             console.log('user is authenticated, trying fetchUserData');
             fetchUserData();
         }
-    }, []);
+    }, [isAuthenticated]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const updatedUser = {
+            username: form.username.value,
+            first_name: form.first_name.value,
+            last_name: form.last_name.value,
+            email: form.email.value,
+            password: form.password.value,
+            _id: userData._id,
+        };
+
+        try {
+            console.log(
+                `Sending userData to updateUser for user ${userData._id}`,
+                updatedUser
+            );
+            await updateUser(updatedUser);
+        } catch (error) {
+            throw new Error('User update failed ', error);
+        }
+    };
+
+    /* 
+    const handleChange = (e) => {
+        setUserData({ ...userData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            console.log('sending userData to updateUser', userData);
+            await updateUser(userData);
+            const updatedData = await fetchCurrentUser();
+            setUserData(updatedData);
+        } catch (error) {
+            console.error('Error updating user: ', error);
+        }
+    }; */
 
     return (
         <div>
             <h1>User Page</h1>
             {userData ? (
-                <div>
-                    <p>Username: {userData.username}</p>
-                    <p>First name: {userData.first_name}</p>
-                    <p>last name: {userData.last_name}</p>
-                    <p>Email: {userData.email}</p>
-                </div>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor='username'>Username:</label>
+                        <input
+                            type='text'
+                            id='username'
+                            name='username'
+                            defaultValue={userData?.username}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='firstName'>First Name:</label>
+                        <input
+                            type='text'
+                            id='first_name'
+                            name='first_name'
+                            defaultValue={userData?.first_name}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='lastName'>Last Name:</label>
+                        <input
+                            type='text'
+                            id='last_name'
+                            name='last_name'
+                            defaultValue={userData?.last_name}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='email'>Email:</label>
+                        <input
+                            type='email'
+                            id='email'
+                            name='email'
+                            defaultValue={userData?.email}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='password'>Password:</label>
+                        <input
+                            type='password'
+                            id='password'
+                            name='password'
+                            placeholder='Enter new passwordd'
+                        />
+                    </div>
+                    <button type='submit'>Update</button>
+                </form>
             ) : (
                 <p>Loading...</p>
             )}
