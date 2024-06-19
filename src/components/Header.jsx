85ef62d8c015f1cb72e4d650b444ab765e187a18
danishkodemonkey <humberjini@hotmail.com>
2024-06-19 13:42:28 +0200

    style pass: add banner rulesets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import NavBar from './Navbar';
 function Header() {
     return (
         <div>
-            <div className='bg-gradient-to-r from-orange-200 from-5% via-orange-400 via-50%  to-orange-200 to-95% text-center h-full  pt-5 w-screen'>
+            <div className='banner'>
                 <h1 className='text-4xl h-full'>The danishKodeMonkey blog</h1>
                 <NavBar />
             </div>
